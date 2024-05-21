f95527b40e7bfac41e562f50caf7d6f2e4588c56
hushmand <hushmand@safarbooking.com>
2024-05-21 12:50:22 +0330

    route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React, {Fragment} from "react";
 import ProductCard from "@/app/components/ProductCard";
+import Link from "next/link";
 export default function Home() {
   return (
     <Fragment>
       <h1 className="">Hello</h1>
+        <Link href='./users'>Users</Link>
         <ProductCard/>
     </Fragment>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Link from "next/link";
 
-const page = () => {
-  return (
-    <div>users page</div>
-  )
+interface User {
+    id: number,
+    name: string
+}
+const page = async () => {
+    const re = await fetch(`https://jsonplaceholder.typicode.com/users`,{cache:'no-store'});
+    const users: User[] = await re.json();
+    return (
+        <div>
+            <h1>Users List</h1>
+            <p>{new Date().toLocaleTimeString('fa-ir')}</p>
+            <ul>
+                {users.map(item => <li key={item.id}>{item.name}</li>)}
+            </ul>
+
+            <Link href='/'>Back</Link>
+        </div>
+    )
 }
 
 export default page
