3c214f4809d177b79c1967fc2909c4fffc036d73
M-SADEGH <hooshmand.wk@gmail.com>
2024-05-25 23:41:09 +0330

    loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 const Loading = () => {
     return (
-        <div>Loading...</div>
+        <div className='flex justify-center my-5'>
+            <span className="loading loading-ring text-blue-500  loading-lg"></span>
+        </div>
     )
 }
 export default Loading
