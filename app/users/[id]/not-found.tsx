551762f00a3f082ce92bedc6d096aa6055eacc6d
M-SADEGH <hooshmand.wk@gmail.com>
2024-05-28 22:51:21 +0330

    nav

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserNotFoundPage = () => {
+  return (
+    <div>
+    <div>user not-found</div>
+    <img className="mask mask-heart" src="https://img.daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg" />
+    </div>
+  )
+}
+export default UserNotFoundPage
