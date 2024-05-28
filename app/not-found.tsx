8c259d8e2384749f31cd3853d7fd9b03cebfd1be
hushmand <hushmand@safarbooking.com>
2024-05-28 10:25:54 +0330

    errors

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,44 @@ import React from 'react'
 const NotFoundPage = () => {
   return (
     <div className='flex flex-col items-center'>
-        404 | Page Not Found | The Requested Page is Invalid!
+        <div className='my-5'>
+            <b className='text-error'>404</b> | Page Not Found !
+        </div>
+
+
+        <div className="flex justify-center gap-1 my-1 w-full">
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+            <kbd className="kbd">?</kbd>
+        </div>
+        <div className="flex justify-center gap-1 my-1 w-full">
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+            <kbd className="kbd">!</kbd>
+        </div>
+        <div className="flex justify-center gap-1 my-1 w-full">
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+            <kbd className="kbd">*</kbd>
+        </div>
     </div>
   )
 }
